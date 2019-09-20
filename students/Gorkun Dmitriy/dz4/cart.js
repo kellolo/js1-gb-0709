e089cff5a058347b5df2e59d8faa16f47367593e
berryllium <gorkundp@yandex.ru>
2019-09-20 22:48:22 +0300

    Каталог отдельно от корзины

--- students/Gorkun Dmitriy/dz4/cart.js
+++ students/Gorkun Dmitriy/dz4/cart.js
@@ -1,3 +1,20 @@
+let arrProducts = []
+let cart = {
+    products: [],
+    getSum: function () {
+        sum = 0
+        for (let key in this.products) { 
+            sum += this.products[key].price * this.products[key].count
+        }
+        return sum
+    },
+    addProduct (product) {
+        product.count = 1
+        current_product = this.products.find(item => item.id == product.id)
+        if (current_product) current_product.count++
+        else this.products.push(product)
+    }
+}
 
 function Product(_id, _name, _category, _price) {
     this.id = _id
@@ -6,30 +23,25 @@ function Product(_id, _name, _category, _price) {
     this.price = _price
 }
 
-function createCart() {
+function createProducts() {
 
     let flag = true
-    let i = 0
     while (flag) {
-        id = +prompt(`Введите id товара`)
+        id = prompt(`Введите id товара`)
         name = prompt(`Введите название товара`)
         category = prompt(`Введите категорию товара`)
         price = +prompt(`Введите стоимость товара`)
-        cart.products[i] = new Product(id, name, category, price)
-        i++
+        arrProducts.push(new Product(id, name, category, price))
         flag = confirm(`Добавить еще товар в корзину?`)
     }
 
 }
 
-cart = {
-    products: [],
-    getSum: function () {
-        sum = 0
-        for (let product in this.products) { sum += this.products[product].price }
-        return sum
-    }
-}
-createCart()
+createProducts()
+console.log(arrProducts)
+cart.addProduct(arrProducts[0])
+cart.addProduct(arrProducts[2])
+cart.addProduct(arrProducts[2])
+cart.addProduct(arrProducts[1])
 console.log(cart.products)
-console.log(`В корзине находятся товары на сумму ${cart.getSum()} рублей`)
+console.log(`В корзине находятся товары на сумму ${cart.getSum()}$`)
