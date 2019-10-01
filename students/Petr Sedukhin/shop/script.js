0191190132d9b8762f48d8de2395370bb03b97c6
Petr Sedukhin <petr-sed@rambler.ru>
2019-10-01 20:08:42 +0300

    поправил корзину

--- students/Petr Sedukhin/shop/script.js
+++ students/Petr Sedukhin/shop/script.js
@@ -2,15 +2,18 @@ const items = ['Notebook', 'Display', 'Keyboard', 'Mouse', 'Phones', 'Microphone
 const prices = [1000, 200, 20, 10, 25, 10, 40]
 const imgs = ['notebook.jpg', 'display.jpg', 'keyboard.jpg', 'mouse.jpg', 'phones.jpg', 'microphone.jpg', 'camera.jpg']
 const ids = [1, 2, 3, 4, 5, 6, 7]
+const products = createDTO ()
 let cart = []
+
+renderProds(products)
 
 function createProduct (index) {
     return {
-        id: ids[index],
+        id: ids [index],
         name: items [index],
         price: prices [index],
         img: imgs [index]
-    }
+    }  
 }
 
 function createDTO () {
@@ -21,42 +24,17 @@ function createDTO () {
     return arr
 }
 
-function pictProds(data) {
-    let el = document.querySelector ('.products')
-    for (let i = 0; i < data.length; i++ ){
-        el.innerHTML += `<div class = 'product-item'>
-        <img src="img/${data[i].img}">
-        <h2>${data[i].name}</h2>
-        <p>price: ${data[i].price}</p>
-        <button class="btn-prod" data-id = "${data[i].id}">Добавить в корзину</button>
+function renderProds() {
+    htmlString = ''
+    products.forEach((el) => {
+        htmlString += `<div class = 'product-item'>
+        <img src="img/${el.img}">
+        <h2>${el.name}</h2>
+        <p>price: ${el.price}</p>
+        <button class="buy-btn" data-id = "${el.id}">Добавить в корзину</button>
         </div>`
-    } 
-    return el
-}
-
-function prodFoCart (prod) {
-    return {
-        id: prod.id,
-        name: prod.name,
-        price: prod.price,
-        img: prod.img,
-        qty: 1,
-        totalPrice: function () {
-            return this.price*this.qty
-        }
-    }
-}
-
-function addToCart(prod){
-    let btnProd = document.querySelectorAll('.btn-prod')
-    for (let i = 0; i < btnProd.length; i++){
-        btnProd[i].addEventListener ('click', function () {
-            id = btnProd[i-1].getAttribute('data-id')
-            console.log(id)
-            cart.push(prodFoCart(prod[id]))
-        })
-    }   
-    return true   
+    })
+    document.querySelector('.products').innerHTML = htmlString
 }
 
 function calcSum (cart) {
@@ -67,10 +45,17 @@ function calcSum (cart) {
     return sum
 }
 
-function showCart(){
-    let btnCart = document.querySelector('.btn-cart')
-    btnCart.addEventListener ('click', function () {
-        document.querySelector ('.cart-block').classList.toggle ('invisible')
-     })
-     return true
+ function addProduct (index) {
+    //добавление в корзину
+    console.log ('Вы добавили в корзину ' + products [index].name)
 }
+
+//всплытие и захват событий
+document.querySelector('.btn-cart').addEventListener ('click', function () {
+    document.querySelector ('.cart-block').classList.toggle ('invisible')})
+    
+document.querySelector ('.products').addEventListener ('click', function (e) {
+    if (e.target.classList.contains ('buy-btn')) {
+        addProduct (+e.target.dataset ['id'] - 1)
+    }
+})
